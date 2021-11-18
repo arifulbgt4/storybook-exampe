50767f3ee4bcbff5206c54d801f8ff7c03fda27a
Ariful islam <arifulbgt4@gmail.com>
2021-11-18 21:45:40 +0600

    fix: component style

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Form, Button, Avatar, List } from 'antd';
+import GoogleMapReact from 'google-map-react';
+import { Form, Button, List, Row, Col } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import 'antd/lib/form/style/css';
 import 'antd/lib/button/style/css';
 import 'antd/lib/avatar/style/css';
 import 'antd/lib/list/style/css';
+import 'antd/lib/card/style/css';
 import axios from 'axios';
 
 import InputField from '../InputField/InputField';
@@ -13,6 +15,7 @@ import styles from './style.module.css';
 
 const CreateForm = () => {
   const [data, setData] = useState([]);
+  const [openResult, setOpenResult] = useState(false);
   const [form] = Form.useForm();
   const onFinish = (values) => {
     console.log('Finish:', values);
@@ -37,39 +40,136 @@ const CreateForm = () => {
     }
   };
 
-  const handleBlur = () => setData([]);
+  const handleBlur = () => setOpenResult(false);
+  const handleFocus = () => setOpenResult(true);
+
+  const companyData = [
+    {
+      title: 'Name',
+      content: 'Magellan AI',
+    },
+    {
+      title: 'Founded Year',
+      content: '2016',
+    },
+    {
+      title: 'Type',
+      content: 'Private',
+    },
+    {
+      title: 'Emplyees',
+      content: '30',
+    },
+    {
+      title: 'Estimated Annual Revenue',
+      content: '$1M - $10M',
+    },
+  ];
 
   return (
     <div className={styles.formWrapper}>
-      <h2>Add a new company</h2>
-      <Form
-        form={form}
-        name='horizontal_login'
-        layout='inline'
-        onFinish={onFinish}
-      >
-        <Form.Item name='company' className={styles.inputField}>
-          <InputField
-            prefix={SearchOutlined}
-            placeholder='Name, website or representative email'
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-        </Form.Item>
-        <Form.Item className={styles.formButtonWrapper} shouldUpdate>
-          {() => (
-            <Button
-              className={styles.formButton}
-              type='primary'
-              htmlType='submit'
-            >
-              Create Company
-            </Button>
-          )}
-        </Form.Item>
-      </Form>
+      <div className={styles.formArea}>
+        <Form
+          className={styles.w100}
+          form={form}
+          name='horizontal_login'
+          layout='inline'
+          onFinish={onFinish}
+        >
+          <h2 className={styles.header}>Add a new company</h2>
+          <Row gutter={16} className={styles.w100}>
+            <Col lg={19} md={16} sm={14} xs={24}>
+              <Form.Item name='company' className={styles.formItem}>
+                <InputField
+                  prefix={SearchOutlined}
+                  placeholder='Name, website or representative email'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  onFocus={handleFocus}
+                />
+              </Form.Item>
+            </Col>
+            <Col lg={5} md={8} sm={10} xs={24}>
+              <Form.Item className={styles.formItem} shouldUpdate>
+                {() => (
+                  <Button
+                    className={styles.formButton}
+                    type='primary'
+                    htmlType='submit'
+                  >
+                    Create Company
+                  </Button>
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
 
-      {data?.length > 0 && <SearchList data={data} />}
+        {openResult && data?.length > 0 && <SearchList data={data} />}
+      </div>
+      <List
+        grid={{ gutter: 24, xs: 1, sm: 2, md: 3, lg: 5, xl: 5, xxl: 5 }}
+        className={styles.infoList}
+        dataSource={companyData}
+        renderItem={(item) => (
+          <List.Item>
+            <div>
+              <h3>{item.title}</h3>
+              <p className={styles.description}>{item.content}</p>
+            </div>
+          </List.Item>
+        )}
+      />
+      <Row gutter={60} className={styles.companyDetails}>
+        <Col lg={16} md={24} style={{ paddingTop: '30px' }}>
+          <Row gutter={16}>
+            <Col span={12} className={styles.location}>
+              <h3>Location</h3>
+              <p className={styles.description}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Commodi, magni laborum
+              </p>
+            </Col>
+            <Col span={12}>
+              <h3>Timezone</h3>
+              <p className={styles.description}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Commodi, magni laborum
+              </p>
+            </Col>
+
+            <Col span={24} className={styles.map}>
+              <GoogleMapReact
+                // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+                defaultCenter={{
+                  lat: 59.95,
+                  lng: 30.33,
+                }}
+                defaultZoom={11}
+              />
+            </Col>
+          </Row>
+        </Col>
+        <Col lg={8} md={24} className={styles.companyContent}>
+          <div>
+            <h3>Description</h3>
+            <p className={styles.description}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos nam
+              ab reprehenderit libero ipsum repudiandae quaerat debitis eos
+              itaque voluptatem quis, vero nemo voluptates perspiciatis. Omnis
+              ab fugiat sapiente consequatur.
+            </p>
+          </div>
+          <div>
+            <h3>Tags</h3>
+            <span className={styles.description}>Marketing & Advertising</span>
+          </div>
+          <div>
+            <h3>Industry</h3>
+            <p className={styles.description}>Advertising</p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/components/SearchList/SearchList.js
+++ src/components/SearchList/SearchList.js
@@ -3,14 +3,17 @@ import { Avatar, List } from 'antd';
 import 'antd/lib/avatar/style/css';
 import 'antd/lib/list/style/css';
 
+import styles from './style.module.css';
+
 const SearchList = (props) => {
   const { data } = props;
   return (
     <List
       itemLayout='horizontal'
       dataSource={data}
+      className={styles.listStyle}
       renderItem={(d) => (
-        <List.Item>
+        <List.Item className={styles.items}>
           <List.Item.Meta
             avatar={<Avatar src={d?.logo} />}
             title={d.name}
